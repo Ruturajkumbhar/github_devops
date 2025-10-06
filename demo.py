ef85077631ceaaf8e8b7b249a3450e1bd7b332f7
Ruturaj Kumbhar <ruturajkmbhar2612@gmail.com>
2025-10-06 11:24:01 +0530

    add commit

--- demo.py
+++ demo.py
@@ -1,2 +1,2 @@
 print("hii i am a ruturaj kumbhar........")
-print("i learn the devops with multicloud course from the naresh it")
+print("i learn the devops with multicloud course from the naresh it") 
